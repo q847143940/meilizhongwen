de49144b6b07f9b8e95e95be8cb901069a8be621
johnny <847143940@qq.com>
2016-09-23 09:52:03 +0800

    页面测试完成

--- src/js/yanzheng.js
+++ src/js/yanzheng.js
@@ -1,50 +1,93 @@
-$().ready(function() {
-// 在键盘按下并释放及提交后验证提交表单
-  $("#signupForm").validate({
-    rules: {
-      firstname: "required",
-      lastname: "required",
-      username: {
-        required: true,
-        minlength: 2
-      },
-      password: {
-        required: true,
-        minlength: 5
-      },
-      confirm_password: {
+
+$(function(){
+				// 表单验证插件
+				$("form").validate({
+
+					// 设置验证规则
+					rules:{
+						user:{required:true,rangelength:[3,10]},
+						email:{email:true,required:true},
+						url:{url:true},
+						tel:{number:true},
+//						password: {required: true,minlength: 5},
+//    			confirm_password: {required: true,minlength: 5,equalTo: "#password"},
+      			confirm_password: {
         required: true,
         minlength: 5,
         equalTo: "#password"
+      },agree: {required:true},
       },
-      email: {
-        required: true,
-        email: true
-      },
-      topic: {
-        required: "#newsletter:checked",
-        minlength: 2
-      },
-      agree: "required"
-    },
-    messages: {
-      firstname: "请输入您的名字",
-      lastname: "请输入您的姓氏",
-      username: {
-        required: "请输入用户名",
-        minlength: "用户名必需由两个字母组成"
-      },
-      password: {
-        required: "请输入密码",
-        minlength: "密码长度不能小于 5 个字母"
-      },
-      confirm_password: {
-        required: "请输入密码",
-        minlength: "密码长度不能小于 5 个字母",
-        equalTo: "两次密码输入不一致"
-      },
-      email: "请输入一个正确的邮箱",
-      agree: "请接受我们的声明",
-      topic: "请选择两个主题"
-    }
-});
+					
+					// 设置提示信息
+					messages:{
+						user:{required:'亲，这是必要输入的'},
+						agree: "请接受我们的声明",
+						password: {required: "请输入密码",minlength: "密码长度不能小于 5 个字母"},
+
+					confirm_password: {
+					        required: "请输入密码",
+					        minlength: "密码长度不能小于 5 个字母",
+					        equalTo: "两次密码输入不一致"
+					      },
+     				email: "请输入一个正确的邮箱",},
+     				
+     				
+     				submitHandler:function(form){
+     					console.log('hah');
+     					var username = $(form).find('#email').val();
+     					var password = $(form).find('#password').val();
+     					var count = 0;
+
+
+     					var user = $.cookie('user');
+     					user = JSON.parse(user);
+
+     					if(user==null){
+     						user=[];
+     					}
+     					for(var i=0;i<user.length;i++){
+     						if(username==user[i].username){
+     							count++;
+     							alert('该youxiang已注册');
+     						}
+     					}
+     					if(count==0){
+     						var jsonUser = {};
+	     					jsonUser.username = username;
+	     					jsonUser.password = password;
+	     					user.push(jsonUser);
+	           				$.cookie('user', JSON.stringify(user), {expires: 7,path:'/'});
+	           				alert('注册成功');
+//	           				location.go(0);
+							location.href="../../index.html";
+     					}
+        }    
+     				
+
+     				
+				});
+				
+
+				$('.login').on('click',function(){
+					var count = 0;
+					var username = $('#login_email').val();
+					var pwd = $('#login_pwd').val();
+
+					var user = $.cookie('user');
+					user = JSON.parse(user);
+					for(var i = 0;i<user.length;i++){
+						if(username==user[i].username&&pwd==user[i].password){
+							count ++;
+//							e.preventDefault()
+							location.href="../../index.html";
+						}
+					}
+					if(count==0){
+						alert('账号或密码有错误');
+					}
+				});
+
+
+				  
+				
+			});
--- src/js/zitiflash.js
+++ src/js/zitiflash.js
@@ -2,9 +2,9 @@ $(function(){
 	
 	//脸部保养
     $('.t2').on('mouseenter','li',function(){
-    	$(this).addClass('animated shake');
+    	$(this).addClass('animated wobble');
  }).on('mouseleave','li',function(){
-  	$(this).removeClass('shake');
+  	$(this).removeClass('wobble');
   })
 //登录注册
     $('.rtop').on('mouseenter','li',function(){
@@ -20,9 +20,9 @@ $(function(){
   })
  /*商品图片*/
  $('.list1').on('mouseenter',function(){
-    	$(this).addClass('animated flip');
+    	$(this).addClass('animated pulse');
  }).on('mouseleave',function(){
-  	$(this).removeClass('flip');
+  	$(this).removeClass('pulse');
   })
  
  //观看全部
@@ -33,14 +33,52 @@ $(function(){
   })
 
 //商品列表货品分类浏览
-  $('.leftlist_1').on('mouseenter','li',function(){
-    	$(this).addClass('animated rubberBand');
- }).on('mouseleave',function(){
-  	$(this).removeClass('rubberBand');
-  })
 //$('.leftlist_1').on('mouseenter','li',function(){
 //  	$(this).addClass('animated rubberBand');
 // }).on('mouseleave',function(){
 //	$(this).removeClass('rubberBand');
 //})
-});
+
+//海蓝之谜
+ $('.bestuser').on('mouseenter',function(){
+    	$(this).addClass('animated rubberBand');
+ }).on('mouseleave',function(){
+  	$(this).removeClass('rubberBand');
+  })
+  //首页大div列表
+ $('.d1').on('mousedown',function(){
+    	$(this).addClass('animated flipInX');
+ }).on('mouseleave',function(){
+  	$(this).removeClass('flipInX');
+  })
+  //列表页大div列表
+ $('.div1').on('mousedown',function(){
+    	$(this).addClass('animated flipInX');
+ }).on('mouseleave',function(){
+  	$(this).removeClass('flipInX');
+  })
+ //优惠红色框
+ $('.cheaper').on('mousedown',function(){
+    	$(this).addClass('animated rubberBand');
+ }).on('mouseleave',function(){
+  	$(this).removeClass('rubberBand');
+  })
+ 
+ 
+   //商品详情页左侧列表
+ $('.u_li').on('mouseover','li',function(){
+    	$(this).addClass('animated rubberBand');
+ }).on('mouseleave','li',function(){
+  	$(this).removeClass('rubberBand');
+  })
+ $('.u6').on('mouseover','li',function(){
+    	$(this).addClass('animated rubberBand');
+ }).on('mouseleave','li',function(){
+  	$(this).removeClass('rubberBand');
+  })
+  $('.u8').on('mouseover','li',function(){
+    	$(this).addClass('animated rubberBand');
+ }).on('mouseleave','li',function(){
+  	$(this).removeClass('rubberBand');
+  })
+});//function结束
